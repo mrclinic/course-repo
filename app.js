25750773a48397bc73ebabf828f9022a7ef75d07
Mohammad.Mohammad <mohammadabmohammad@gmail.com>
2026-07-29 22:05:05 +0300

    check branching

--- app.js
+++ app.js
@@ -123,6 +123,14 @@ let _lastName = "Johnson";
 let _x = 2;
 let _100 = 5;
 
+
+
+
+
+
+
+
+
 //Note
 //Using the underscore is not very common in JavaScript,
 // but a convention among professional programmers is to use it as an alias for "private (hidden)" variables.
